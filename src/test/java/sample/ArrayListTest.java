ebe4a0cd07834154e6e945165125ce19bd1f2a53
irof <@irof>
2011-10-02 02:19:53 +0900

    ArrayListTest remove by index

--- src/test/java/sample/ArrayListTest.java
+++ src/test/java/sample/ArrayListTest.java
@@ -1,7 +1,7 @@
 package sample;
 
-import static org.hamcrest.CoreMatchers.*;
-import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
 
 import java.util.ArrayList;
@@ -25,5 +25,15 @@ public class ArrayListTest {
 		list.add(new Object());
 
 		assertThat(1, is(list.size()));
+	}
+
+	@Test
+	public void testIndexRemove() throws Exception {
+		ArrayList<Object> list = new ArrayList<Object>();
+		list.add(new Object());
+
+		list.remove(0);
+
+		assertThat(0, is(list.size()));
 	}
 }
