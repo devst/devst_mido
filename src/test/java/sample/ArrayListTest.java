a7cf5f81d53cf3c6cb3a07138119460193ae88f4
irof <@irof>
2011-10-02 02:15:28 +0900

    ArrayListTest add

--- src/test/java/sample/ArrayListTest.java
+++ src/test/java/sample/ArrayListTest.java
@@ -1,6 +1,7 @@
 package sample;
 
 import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
 import static org.junit.Assert.assertThat;
 
 import java.util.ArrayList;
@@ -17,4 +18,12 @@ public class ArrayListTest {
 		assertThat(0, is(list.size()));
 	}
 
+	@Test
+	public void testAdd() throws Exception {
+		ArrayList<Object> list = new ArrayList<Object>();
+
+		list.add(new Object());
+
+		assertThat(1, is(list.size()));
+	}
 }
